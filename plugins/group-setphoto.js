7f6f3bddb71e6b36c90ec04d57050c2152bba3f8
sprohub <kaisonayeito@gmail.com>
2026-06-06 01:57:28 -0500

    Actualizar group-setphoto.js

--- plugins/group-setphoto.js
+++ plugins/group-setphoto.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
-  if (!m.isGroup) return conn.sendMessage(m.chat, { text: '👥 「 HINATA SETPHOTO 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Solo para grupos\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
-  if (!isAdmin) return conn.sendMessage(m.chat, { text: '👥 「 HINATA SETPHOTO 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Solo administradores\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
-  if (!isBotAdmin) return conn.sendMessage(m.chat, { text: '👥 「 HINATA SETPHOTO 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ La bot necesita ser admin\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+  if (!m.isGroup) return conn.sendMessage(m.chat, { text: '👥 「 SAITAMA SETPHOTO 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Solo para grupos\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+  if (!isAdmin) return conn.sendMessage(m.chat, { text: '👥 「 SAITAMA SETPHOTO 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Solo administradores\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+  if (!isBotAdmin) return conn.sendMessage(m.chat, { text: '👥 「 SAITAMA SETPHOTO 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ La bot necesita ser admin\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
 
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!/image/.test(mime)) return conn.sendMessage(m.chat, { text: '👥 「 HINATA SETPHOTO 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Responde a una imagen\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+  if (!/image/.test(mime)) return conn.sendMessage(m.chat, { text: '👥 「 SAITAMA SETPHOTO 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Responde a una imagen\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
 
   let img = await q.download()
   await conn.updateProfilePicture(m.chat, img)
-  await conn.sendMessage(m.chat, { text: '👥 「 HINATA SETPHOTO 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n✅ » Foto del grupo actualizada\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+  await conn.sendMessage(m.chat, { text: '👥 「 SAITAMA SETPHOTO 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n✅ » Foto del grupo actualizada\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
 }
 
 handler.help = ['setphoto']
