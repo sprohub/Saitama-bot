5438c73118b2f1d0d527a58e360df28d99a6e280
sprohub <kaisonayeito@gmail.com>
2026-06-05 23:08:59 -0500

    Actualizar owner-addiamantes.js

--- plugins/owner-addiamantes.js
+++ plugins/owner-addiamantes.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, args }) => {
   let who = m.sender
-  let owners = ['59177474230@s.whatsapp.net', '573223090406@s.whatsapp.net']
+  let owners = ['59177474230@s.whatsapp.net', '573223090406@s.whatsapp.net',
+'573225396540@s.whatsapp.net']
 
   if (!owners.includes(who)) {
     return conn.sendMessage(m.chat, {
