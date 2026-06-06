9c0599226e06ffbbd0387ef11d3d283c86babd5e
sprohub <kaisonayeito@gmail.com>
2026-06-05 23:06:24 -0500

    Actualizar owner-gachafull.js

--- plugins/owner-gachafull.js
+++ plugins/owner-gachafull.js
@@ -3,7 +3,8 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
   let who = m.sender
-  let owners = ['59177474230@s.whatsapp.net', '573223090406@s.whatsapp.net']
+  let owners = ['59177474230@s.whatsapp.net', '573223090406@s.whatsapp.net',
+'573225396540@s.whatsapp.net',]
 
   if (!owners.includes(who)) {
     return conn.sendMessage(m.chat, {
