aaa649ba9ae7df772a1bbdca88b1f6aa771f98e9
sprohub <kaisonayeito@gmail.com>
2026-06-12 20:31:01 -0500

    Actualizar prin-menu.js

--- plugins/prin-menu.js
+++ plugins/prin-menu.js
@@ -18,40 +18,41 @@ const tags = {
 }
 
 const bannerCategory = {
-  main: 'https://i.ibb.co/ksyfSMhm/images.jpg',
-  group: 'https://i.ibb.co/R4PjYXDs/mar.jpg',
-  rpg: 'https://i.ibb.co/7xW3Lfy3/paramo.jpg',
-  game: 'https://i.ibb.co/ksyfSMhm/images.jpg',
-  gacha: 'https://i.ibb.co/ksyfSMhm/images.jpg',
-  serbot: 'https://i.ibb.co/ksyfSMhm/images.jpg',
-  owner: 'https://i.ibb.co/7xW3Lfy3/paramo.jpg',
-  downloader: 'https://i.ibb.co/R4PjYXDs/mar.jpg',
-  info: 'https://i.ibb.co/7xW3Lfy3/paramo.jpg',
-  diversion: 'https://i.ibb.co/ksyfSMhm/images.jpg',
-  anime: 'https://i.ibb.co/ksyfSMhm/images.jpg'
+  main: 'https://i.ibb.co/TB7cZfFG/SAITAMAmenu.jpg',
+  group: 'https://i.ibb.co/C38P3Wqg/ultra.jpg',
+  rpg: 'https://i.ibb.co/V040CGfq/enojado.jpg',
+  game: 'https://i.ibb.co/jkhp8BZD/wof.jpg',
+  gacha: 'https://i.ibb.co/DPHT5V5Y/caminata.jpg',
+  serbot: 'https://i.ibb.co/j94w01QV/mascota.jpg',
+  owner: 'https://i.ibb.co/V040CGfq/enojado.jpg',
+  downloader: 'https://i.ibb.co/C38P3Wqg/ultra.jpg',
+  info: 'https://i.ibb.co/jkhp8BZD/wof.jpg',
+  diversion: 'https://i.ibb.co/j94w01QV/mascota.jpg',
+  anime: 'https://i.ibb.co/DPHT5V5Y/caminata.jpg'
 }
 
 const defaultMenu = {
   before: `
-╔════════════════╗
-THORFINN-BOT
-╚════════════════╝
+╭━━⬣ *SAITAMA-BOT* ⬣━━╮
+
 〖 %totalreg ᴜꜱᴇʀꜱ 〗 %totalcmd ᴄᴍᴅꜱ ➣
 
 > ⏱️ %uptime activa
 > 👤 Solicitado por @%user
 
+╰━━━━━━━━━━━━━━━━━━━━━━⬣
+
 %readmore
 `,
-  header: '\n➣ %category ➣ (%count cmd)\n',
+  header: '\n╭━━⬣ %category ➣ (%count cmd)\n╰━━━━━━━━━━━━━━━━━━━━━━⬣\n',
   body: '➣ %cmd',
   desc: '\n> ↆ %desc',
   footer: '',
   after: `
 
-╔═════════════════╗
-   samu★ ➣THOFINN
-╚═════════════════╝`
+╭━━━━━━━━━━━━━━━━━━━━━━⬣
+   samu★ ➣ SAITAMA-BOT
+╰━━━━━━━━━━━━━━━━━━━━━━⬣`
 }
 
 let handler = async (m, { conn, usedPrefix: _p, command }) => {
@@ -83,7 +84,7 @@ let handler = async (m, { conn, usedPrefix: _p, command }) => {
       }
     }
 
-    let bannerFinal = tagSeleccionada ? bannerCategory[tagSeleccionada] : 'https://i.ibb.co/ksyfSMhm/images.jpg'
+    let bannerFinal = tagSeleccionada ? bannerCategory[tagSeleccionada] : bannerCategory.main
 
     let textoMenu = defaultMenu.before
       .replace(/%totalreg/g, Object.keys(global.db.data.users).length)
@@ -92,7 +93,7 @@ let handler = async (m, { conn, usedPrefix: _p, command }) => {
       .replace(/%user/g, who.split('@')[0])
 
     if (tagSeleccionada) {
-      textoMenu = textoMenu.replace('saitamabot', 'saitamabot ➣ ' + tags[tagSeleccionada].replace(/[⭐👥⚔️🎮🎰🤖👑📥ℹ️]/g, '').trim())
+      textoMenu = textoMenu.replace('SAITAMA-BOT', 'SAITAMA-BOT ➣ ' + tags[tagSeleccionada].replace(/[⭐👥⚔️🎮🎰🤖👑📥ℹ️🎪🌸]/g, '').trim())
     }
 
     for (let tag of Object.keys(tags)) {
