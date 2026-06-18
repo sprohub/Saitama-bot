a67dd2a016ec611650e11a406a92c58214add85a
sprohub <kaisonayeito@gmail.com>
2026-06-18 18:50:56 -0500

    Actualizar diversion-pp.js

--- plugins/diversion-pp.js
+++ plugins/diversion-pp.js
@@ -1,20 +1,39 @@
 let handler = async (m, { conn }) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
 
+  let name = '@' + who.split('@')[0]
+
   let pp
   try {
     pp = await conn.profilePictureUrl(who, 'image')
   } catch {
+    let texto = '╭━━⬣ *SAITAMA* ⬣\n'
+    texto += '┃\n'
+    texto += '┃ 💫 *SIN FOTO DE PERFIL*\n'
+    texto += '┃\n'
+    texto += '┃ 👤 ' + name + '\n'
+    texto += '┃ ❌ No tiene foto pública\n'
+    texto += '┃\n'
+    texto += '┃ 「Ni siquiera eso pude golpear...」\n'
+    texto += '┃\n'
+    texto += '╰━━━━━━━━━━━━━━━━━━━━━━⬣ *SAITAMA*'
+
     return conn.sendMessage(m.chat, {
-      text: '𖣔 「 HINATA PP 」 ˚ʚ♡ɞ˚\n\n💫 » No tiene foto de perfil'
+      text: texto,
+      mentions: [who]
     }, { quoted: m })
   }
 
-  let name = '@' + who.split('@')[0]
-
-  let texto = '𖣔 「 HINATA PP 」 ˚ʚ♡ɞ˚\n\n'
-  texto += '📸 » ' + name + '\n'
-  texto += '> Foto de perfil robada 😈'
+  let texto = '╭━━⬣ *SAITAMA* ⬣\n'
+  texto += '┃\n'
+  texto += '┃ 📸 *FOTO ROBADA*\n'
+  texto += '┃\n'
+  texto += '┃ 👤 ' + name + '\n'
+  texto += '┃ 😈 Objetivo capturado\n'
+  texto += '┃\n'
+  texto += '┃ 「Un golpe. Una foto.」\n'
+  texto += '┃\n'
+  texto += '╰━━━━━━━━━━━━━━━━━━━━━━⬣ *SAITAMA*'
 
   await conn.sendMessage(m.chat, {
     image: { url: pp },
