8c23908fc9e249f0072bbd635c8b47ac1c67670c
sprohub <kaisonayeito@gmail.com>
2026-07-08 22:33:10 -0500

    Actualizar group-ship.js

--- plugins/group-ship.js
+++ plugins/group-ship.js
@@ -21,32 +21,38 @@ let handler = async (m, { conn }) => {
 
   let porcentaje = Math.floor(Math.random() * 101)
 
-  let emoji, frase, extra
+  let emoji, frase, extra, imagen
 
   if (porcentaje >= 90) {
     emoji = '💘'
     frase = 'Almas gemelas'
     extra = '¡Un golpe de amor de un solo puñetazo! Se casan mañana 💒'
+    imagen = 'https://i.ibb.co/CKrTJVnd/90-100-Almas-gemelas.png'
   } else if (porcentaje >= 70) {
     emoji = '💖'
     frase = 'Amor fuerte'
     extra = 'Hay mucha química entre ustedes, denle una oportunidad 💕'
+    imagen = 'https://i.ibb.co/7HXx28X/70-89-Amorfuerte.png'
   } else if (porcentaje >= 50) {
     emoji = '💗'
     frase = 'Posible romance'
     extra = 'Podría funcionar si se conocen mejor 🌸'
+    imagen = 'https://i.ibb.co/JWBXBxLR/50-69-Posibleromance.png'
   } else if (porcentaje >= 30) {
     emoji = '💛'
     frase = 'Amistad'
     extra = 'Mejor como amigos, no arruinen la amistad 🤝'
+    imagen = 'https://i.ibb.co/Cp6Gw0rF/30-49-Amistad.png'
   } else if (porcentaje >= 10) {
     emoji = '💔'
     frase = 'Difícil'
     extra = 'No hay química, mejor busca en otro lado 🥀'
+    imagen = 'https://i.ibb.co/twnZvWsg/5fe9f416-9fdc-4e8f-b0bd-9719d9286073.png'
   } else {
     emoji = '🖤'
     frase = 'Imposible'
     extra = 'Ni Saitama con un puñetazo arregla esto, huye de ahí 🏃'
+    imagen = 'https://i.ibb.co/6RCxbYq4/0-9-Imposible.png'
   }
 
   let barra = ''
@@ -63,7 +69,11 @@ let handler = async (m, { conn }) => {
   texto += '├ 📝 » ' + extra + '\n'
   texto += '╰───────────────⬣'
 
-  await conn.sendMessage(m.chat, { text: texto, mentions: [user1, user2] }, { quoted: m })
+  await conn.sendMessage(m.chat, {
+    image: { url: imagen },
+    caption: texto,
+    mentions: [user1, user2]
+  }, { quoted: m })
 }
 
 handler.help = ['ship']
