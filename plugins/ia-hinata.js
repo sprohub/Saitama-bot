8928782223826d8c0b2298b2d1f83e8c49048d0f
sprohub <kaisonayeito@gmail.com>
2026-07-09 00:42:33 -0500

    Update GROQ_KEY in ia-hinata.js

--- plugins/ia-hinata.js
+++ plugins/ia-hinata.js
@@ -1,4 +1,4 @@
-const GROQ_KEY = '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'
+const GROQ_KEY = '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'
 const GROQ_URL = 'https://api.groq.com/openai/v1/chat/completions'
 
 const VIGILANTE_NUM = '573225396540'
