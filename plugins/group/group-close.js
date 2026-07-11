9723f18a7c911b43d2eff39b49a1b6ed0fab135b
sprohub <kaisonayeito@gmail.com>
2026-07-11 08:35:13 -0500

    Actualizar group-close.js

--- plugins/group/group-close.js
+++ plugins/group/group-close.js
@@ -1,17 +1,17 @@
-let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
-  if (!m.isGroup) return conn.sendMessage(m.chat, { text: '👥 「 HINATA CLOSE 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Solo para grupos\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
-  if (!isAdmin) return conn.sendMessage(m.chat, { text: '👥 「 HINATA CLOSE 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Solo administradores\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
-  if (!isBotAdmin) return conn.sendMessage(m.chat, { text: '👥 「 HINATA CLOSE 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ La bot necesita ser admin\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+let handler = async (m, { conn, isOwner, isBotAdmin }) => {
+  if (!m.isGroup) return conn.sendMessage(m.chat, { text: '╭─⪼ 🌿 *SAITAMA-BOT*\n│ 🍃 Solo para grupos\n╰───────────────⬣' }, { quoted: m })
+  if (!isOwner) return conn.sendMessage(m.chat, { text: '╭─⪼ 🌿 *SAITAMA-BOT*\n│ 🍃 Solo el owner puede usar este comando\n╰───────────────⬣' }, { quoted: m })
+  if (!isBotAdmin) return conn.sendMessage(m.chat, { text: '╭─⪼ 🌿 *SAITAMA-BOT*\n│ 🍃 La bot necesita ser admin\n╰───────────────⬣' }, { quoted: m })
 
   await conn.groupSettingUpdate(m.chat, 'announcement')
-  await conn.sendMessage(m.chat, { text: '👥 「 HINATA CLOSE 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n🔒 » Grupo cerrado\n🔇 » Solo admins hablan\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+  await conn.sendMessage(m.chat, { text: '╭─⪼ 🌿 *SAITAMA-BOT*\n│ 🍃 Grupo cerrado, solo admins hablan\n╰───────────────⬣' }, { quoted: m })
 }
 
 handler.help = ['close']
 handler.tags = ['group']
 handler.command = /^(close|cerrar)$/i
 handler.desc = 'Cierra el grupo'
-handler.admin = true
+handler.owner = true
 handler.botAdmin = true
 
 export default handler
