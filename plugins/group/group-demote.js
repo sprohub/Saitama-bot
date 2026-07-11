a9cdb6fcae1d9cfea544c4b7a00628379766486e
sprohub <kaisonayeito@gmail.com>
2026-07-11 08:29:54 -0500

    Actualizar group-demote.js

--- plugins/group/group-demote.js
+++ plugins/group/group-demote.js
@@ -1,19 +1,19 @@
-let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
-  if (!m.isGroup) return conn.sendMessage(m.chat, { text: '👥 「 HINATA DEMOTE 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Solo para grupos\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
-  if (!isAdmin) return conn.sendMessage(m.chat, { text: '👥 「 HINATA DEMOTE 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Solo administradores\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
-  if (!isBotAdmin) return conn.sendMessage(m.chat, { text: '👥 「 HINATA DEMOTE 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ La bot necesita ser admin\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+let handler = async (m, { conn, isOwner, isBotAdmin }) => {
+  if (!m.isGroup) return conn.sendMessage(m.chat, { text: '╭─⪼ 🌿 *SAITAMA-BOT*\n│ 🍃 Solo para grupos\n╰───────────────⬣' }, { quoted: m })
+  if (!isOwner) return conn.sendMessage(m.chat, { text: '╭─⪼ 🌿 *SAITAMA-BOT*\n│ 🍃 Solo el owner puede usar este comando\n╰───────────────⬣' }, { quoted: m })
+  if (!isBotAdmin) return conn.sendMessage(m.chat, { text: '╭─⪼ 🌿 *SAITAMA-BOT*\n│ 🍃 La bot necesita ser admin\n╰───────────────⬣' }, { quoted: m })
 
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : null
-  if (!who) return conn.sendMessage(m.chat, { text: '👥 「 HINATA DEMOTE 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Menciona o responde a quien quitar admin\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+  if (!who) return conn.sendMessage(m.chat, { text: '╭─⪼ 🌿 *SAITAMA-BOT*\n│ 🍃 Menciona o responde a quien quitar admin\n╰───────────────⬣' }, { quoted: m })
 
   try {
     await conn.groupParticipantsUpdate(m.chat, [who], 'demote')
     await conn.sendMessage(m.chat, { 
-      text: '👥 「 HINATA DEMOTE 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n✅ » @' + who.split('@')[0] + ' ya no es admin\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔',
+      text: '╭─⪼ 🌿 *SAITAMA-BOT*\n│ 🍃 @' + who.split('@')[0] + ' ya no es admin\n╰───────────────⬣',
       mentions: [who]
     }, { quoted: m })
   } catch (e) {
-    await conn.sendMessage(m.chat, { text: '👥 「 HINATA DEMOTE 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Error al quitar admin\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+    await conn.sendMessage(m.chat, { text: '╭─⪼ 🌿 *SAITAMA-BOT*\n│ 🍃 Error al quitar admin\n╰───────────────⬣' }, { quoted: m })
   }
 }
 
@@ -21,7 +21,7 @@ handler.help = ['demote']
 handler.tags = ['group']
 handler.command = /^(demote|degradar|quitaradmin)$/i
 handler.desc = 'Quita administrador'
-handler.admin = true
+handler.owner = true
 handler.botAdmin = true
 
 export default handler
