7e940f804e05c28006f7bab6fb865536b0edca12
BrayanRK <brayangasca37@gmail.com>
2026-07-15 23:41:58 -0500

    Fix: hardcodear version de Baileys para evitar error 405 y agregar .gitignore
    - fetchLatestWaWebVersion() causaba Connection Closed al vincular
    - se centraliza la version correcta en utils/_version.js (ESM)
    - se corrige script start en package.json (build-edw.cjs no existia)
    - se agrega .gitignore para excluir sessions, node_modules, settings locales

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ global.loadDatabase = async function loadDatabase() {
 global.authFile = `sessions`;
 const { state, saveCreds } = await useMultiFileAuthState(global.authFile);
 
-const { version } = await fetchLatestWaWebVersion();
+const { BAILEYS_VERSION } = await import('./utils/_version.js'); const version = BAILEYS_VERSION;
 
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 const question = (texto) => new Promise((resolver) => rl.question(texto, resolver));
