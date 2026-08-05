342585eccae7e59a8b274318e6c5e9c22d42d097
sprohub <kaisonayeito@gmail.com>
2026-08-04 23:39:08 -0500

    Actualizar dls-mega.js

--- plugins/media/dls-mega.js
+++ plugins/media/dls-mega.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-const API_URL = 'https://api.delirius.store/download/mega'
+const API_URL = 'https://api.delirius.online/download/mega'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   const raw = text?.trim()
